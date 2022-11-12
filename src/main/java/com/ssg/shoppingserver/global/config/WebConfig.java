debff55a17c22513dfc0837f1909111c3bbd7d5c
JeongSangByuk <jsb100800@naver.com>
2022-11-13 07:50:56 +0900

    fix : user search api type change

---
+++ src/main/java/com/ssg/shoppingserver/global/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.chicplay.mediaserver.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.http.HttpSessionListener;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("https://dev.edu-vivid.com","http://localhost:8081")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+}
